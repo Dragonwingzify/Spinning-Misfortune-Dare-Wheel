65a1df855a32c7178feedb464fba89281eef9472
Dragonwingzify <cake_dust@hotmail.com>
2024-04-09 19:08:50 +0200

    Delete challenge
    - Implemented a function that should be able to delete each individual challenge.

--- punish-wheel/src/components/inputField.jsx
+++ punish-wheel/src/components/inputField.jsx
@@ -10,7 +10,7 @@ export default function InputField({
       type={type}
       onChange={onChange}
       value={title}
-      className="peer block w-full border-0 bg-gray-50 py-1.5 px-20 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
+      className="peer block w-full border-2 bg-gray-50 py-1.5 px-20 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
       placeholder={placeholder}
       disabled={disabled}
     />
