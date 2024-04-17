cf60b0210b10fa0aa767405b3ca021e9e5f7dc5d
Dragonwingzify <cake_dust@hotmail.com>
2024-04-17 18:57:27 +0200

    Update history
    - Displaying the total count of how many times we have landed on a spin in the history tab.
    
    - removed some logs and some functionality that I didn't use.

--- punish-wheel/src/components/spinDetails.jsx
+++ punish-wheel/src/components/spinDetails.jsx
@@ -5,7 +5,9 @@ export default function SpinDetails({ spin, data }) {
         <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-0">
           {spin.title}
         </td>
-        <td className="whitespace-nowrap p-4 text-sm text-gray-500">{data}</td>
+        <td className="whitespace-nowrap p-4 text-sm font-medium text-gray-900">
+          {spin.totalWheelSpin}
+        </td>
       </tr>
     </>
   );
